19dd22dd666a3027dee16309b571da3bee1b573f
[fatemeh noorali] <[vesal.noorali@gmail.com]>
2024-08-10 17:43:59 +0330

    error handling

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './all-exceptions.filter';
 // import { MyLoggerService } from './my-logger/my-logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   // app.useLogger(app.get(MyLoggerService));
   app.enableCors(); // this opens up for everyone
   app.setGlobalPrefix('api');
